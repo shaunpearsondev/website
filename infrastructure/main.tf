4db8de8ac683419297404f41e41ef2a0c87296fe
Shaun Pearson <mail@shaunpearson.dev>
2021-03-21 21:56:49 +0000

    Move state file

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,11 @@
-# terraform {
-#   backend "azurerm" {}
-# }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "shaunpearsonstate"
+    storage_account_name = "shaunpearsonstate"
+    container_name       = "shaunpearsonstate"
+    key                  = "shaunpearsondev.tfstate"
+  }
+}
 
 resource "azurerm_resource_group" "rg" {
   name     = "website"
@@ -8,14 +13,14 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "static_storage" {
-  name                     = "shaunpearsondev"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_kind             = "StorageV2"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  enable_https_traffic_only = true 
-  min_tls_version = "TLS1_2"  
+  name                      = "shaunpearsondev"
+  resource_group_name       = azurerm_resource_group.rg.name
+  location                  = azurerm_resource_group.rg.location
+  account_kind              = "StorageV2"
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  enable_https_traffic_only = true
+  min_tls_version           = "TLS1_2"
 
   static_website {
     index_document = "index.html"
--- infrastructure/versions.tf
+++ infrastructure/versions.tf
@@ -1,4 +1,4 @@
 provider "azurerm" {
-    subscription_id = "20d652cb-1a12-4ba3-b652-86cf13399fe6"
-    features {}
+  subscription_id = "20d652cb-1a12-4ba3-b652-86cf13399fe6"
+  features {}
 }
